e2bca05a50a4e0183bc11a3521a00680431228e4
Rahin Haque <haquerahin743@gmail.com>
2026-06-09 23:07:09 +0600

    Added static sections to the Homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,23 @@
+import Hero from "@/components/Hero";
+import Statistics from "@/components/Statistics";
+import Testimonials from "@/components/Testimonials";
+import WhyChoose from "@/components/WhyChoose";
 
 export default async function HomePage() {
 
+  const stats = {
+    totalEvents: 40,
+    totalAttendees: 4000,
+    totalOrgs: 20,
+  };
 
   return (
     <div>
-      Welcome to Ticketo.
+      <Hero />
+
+      <WhyChoose />
+      <Statistics stats={stats} />
+      <Testimonials />
     </div>
   );
 }
-
